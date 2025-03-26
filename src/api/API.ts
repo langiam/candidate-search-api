8d84d65fb32703e83a738ea5cb8e9564d1f4c6f6
langiam <ryan.matthew.lang@gmail.com>
2025-03-26 11:32:36 -0400

    added api ts and axios module

---
+++ src/api/API.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+import { GitHubUser } from "../interfaces/GitHubUser";
+
+const API_BASE = "https://api.github.com/users";
+
+export const getCandidate = async (username: string): Promise<GitHubUser> => {
+  const res = await axios.get(`${API_BASE}/${username}`, {
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
+    },
+  });
+  return res.data;
+};
---
+++ src/interfaces/GitHubUser.ts
@@ -0,0 +1,11 @@
+export interface GitHubUser {
+    login: string;
+    name: string;
+    location: string;
+    avatar_url: string;
+    email: string;
+    html_url: string;
+    company: string;
+    bio: string;
+  }
+  
